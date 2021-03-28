cb78844d24f23406a0dc2ce080b96ef4e17506bf
Isaac Feldman <isaaccfeldman@gmail.com>
2021-03-28 16:56:34 -0400

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+DOTFILES_DIR=$(pwd)
+CONFIG=$DOTFILES_DIR/config
+
 # Create symlinks
-ln -s -f ./.vimrc ~/.vimrc
-ln -s -f ./.bashrc ~/.bashrc
+ln -s -f $CONFIG/vim/.vimrc ~/.vimrc
+ln -s -f $CONFIG/bash/.bashrc ~/.bashrc
+
+
+# Setup Newsboat
+if ! [ -d ~/.newsboat ]; then
+	echo "Newsboat not installed; aborting..."
+	exit 1
+fi
+
+ln -s -f $CONFIG/newsboat/config ~/.newsboat/config
+ln -s -f $CONFIG/newsboat/urls ~/.newsboat/urls
 
 echo "Done!"
 exit 0
